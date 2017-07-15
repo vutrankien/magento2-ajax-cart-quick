357d3289116be0e8eb78bcdcc07cf06d05a01d95
Vũ Trần Kiên <wereasda3@gmail.com>
2017-07-15 14:10:44 +0700

    Upload files
    Upload files

---
+++ app/code/Magebay/AjaxCartPro/Block/AjaxCart.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Copyright © 2016 Magebay. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magebay\AjaxCartPro\Block;
+class AjaxCart extends \Magento\Framework\View\Element\Template
+{
+	public function __construct(
+		\Magento\Framework\View\Element\Template\Context $context,
+		array $data = []
+	){	
+		 parent::__construct($context, $data);
+	}
+	
+	
+	
+}
---
+++ app/code/Magebay/AjaxCartPro/view/frontend/requirejs-config.js
@@ -0,0 +1,13 @@
+var config = {
+    map: {
+        '*': {
+			magebaySidebar: 'Magebay_AjaxCartPro/js/sidebar',
+            catalogAddToCart: 'Magebay_AjaxCartPro/js/catalog-add-to-cart',
+			catalogAddToCompare: 'Magebay_AjaxCartPro/js/catalog-add-to-compare'
+        },
+		'shim': {
+    		'Magebay_AjaxCartPro/js/catalog-add-to-cart': ['catalogAddToCart'],
+			'Magebay_AjaxCartPro/js/catalog-add-to-compare': ['mage/dataPost']
+    	}
+    }
+};
